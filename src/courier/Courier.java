1d7bd7b4eb5933f4e0ae72337e5c44f32fc7dacf
Chui Yee <ccy030300@gmail.com>
2020-09-22 12:44:10 +0800

    added hi

--- src/courier/Courier.java
+++ src/courier/Courier.java
@@ -5,6 +5,7 @@ public class Courier {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, World!");
+        System.out.println("hi");
     }
     
 }
