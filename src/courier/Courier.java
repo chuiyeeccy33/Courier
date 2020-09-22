88f3d033a606c84e926bfd596d49e597b3b2a444
daniellim051000 <daniellim0510@pop-os.localdomain>
2020-09-22 12:19:13 +0800

    delete testing print

--- src/courier/Courier.java
+++ src/courier/Courier.java
@@ -17,7 +17,6 @@ public class Courier {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, World!");
-        System.out.println("testing");
     }
     
 }
