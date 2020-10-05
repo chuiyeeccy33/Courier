8bc8f402ecd935e005b20332110e45c818349e71
Chui Yee <ccy030300@gmail.com>
2020-10-05 15:01:13 +0800

    delete bullshit

--- src/courier/Courier.java
+++ src/courier/Courier.java
@@ -4,9 +4,6 @@ public class Courier {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello, World!");
-        System.out.println("hi");
-        //test
     }
     
 }
